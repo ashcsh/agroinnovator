b6d3ed1d3373d8b4824da732e7b7874e750d58bd
ashcsh <cvictorpodgoreanu@gmail.com>
2023-03-06 06:18:38 +0200

    3d

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 
 import phonewave from "./assets/phonewave.svg";
 import logo from "./assets/logo.png";
+
+import { Suspense } from 'react'
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import  Satelit  from "./components/gltf/satelitGLTF.jsx";
 function App() {
   return (
     <>
@@ -9,10 +14,21 @@ function App() {
         <img id="logo" src={logo} alt="logo" />
       </nav>
       <img className="wave" src={phonewave} alt="wave" />
-      <div className="section1"></div>
-      <div className="section2"></div>
+      <div className="section1">
+       
+      </div>
+      <div className="section2">
+      <div className="earth">
+          <Canvas position={[100, 100, 0]}>
+            <Suspense> 
+            <ambientLight position={[-100, -100, -100]} intensity={3} /> 
+               <Satelit /> 
+              <OrbitControls enableRotate={true} enableZoom={false} /> 
+            </Suspense>
+          </Canvas> 
+        </div>
+      </div>
       <div className="section3"></div>
-      
     </>
   );
 }
