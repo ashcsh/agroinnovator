3202eaa71b61acce7c9d9b49785ed4bb1fc25948
ashcsh <cvictorpodgoreanu@gmail.com>
2023-03-06 18:43:35 +0200

    sat

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import "./App.css";
 import phonewave from "./assets/phonewave.svg";
 import logo from "./assets/logo.png";
 
-import { Suspense } from 'react'
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
-import  Satelit  from "./components/gltf/satelitGLTF.jsx";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Satelit from "./components/gltf/Artemis.jsx";
 function App() {
   return (
     <>
@@ -15,19 +15,26 @@ function App() {
       </nav>
       <img className="wave" src={phonewave} alt="wave" />
       <div className="section1">
-       
-      </div>
-      <div className="section2">
-      <div className="earth">
-          <Canvas position={[100, 100, 0]}>
-            <Suspense> 
-            <ambientLight position={[-100, -100, -100]} intensity={3} /> 
-               <Satelit /> 
-              <OrbitControls enableRotate={true} enableZoom={false} /> 
+        <h1 id="introtext">
+          Sustinem dezvoltarea agriculturii sustenabile si performate.
+        </h1>
+
+        <div className="earth">
+          <Canvas position={[0,0, 0]}>
+            <Suspense>
+              <ambientLight position={[-100, -100, -100]} intensity={1.5} />
+              <Satelit />
+              <OrbitControls enableRotate={true} enableZoom={false} />
             </Suspense>
-          </Canvas> 
+          </Canvas>
+        </div>
+        <div className="butoane">
+          <h1>misiune</h1>
+          <h1>viziune</h1>
+          <h1>membrii</h1>
         </div>
       </div>
+      <div className="section2"></div>
       <div className="section3"></div>
     </>
   );
